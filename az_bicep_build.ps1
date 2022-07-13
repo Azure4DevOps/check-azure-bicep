6266d3bc9841f6f9c3a0fa103087d30bcce312da
Janusz Nowak <janono@gmail.com>
2022-07-13 23:26:25 +0200

    Change script path

---
+++ az_bicep_build.ps1
@@ -0,0 +1,16 @@
+#$in = Get-ChildItem -Recurse -Filter "*.bicep"
+$in = $args
+
+$error_azbuild = $false;
+az bicep version
+foreach ($b in $in) {
+    $a = az bicep build --stdout --file $b
+    if ( $LASTEXITCODE -eq 0 ) {
+    }
+    else {
+        $error_azbuild = $true
+    }
+}
+if ( $error_azbuild -eq $true ) {
+    Exit 25
+}
