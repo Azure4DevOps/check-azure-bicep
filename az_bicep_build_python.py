e01dcaf37c2d29695e4b8f0c64ef1a4c36b72243
Janusz Nowak <janono@gmail.com>
2022-08-18 00:12:26 +0200

    #10  Use Only Python and additional_dependencies: [azure-cli, subprocess.run]

---
+++ az_bicep_build_python.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+import glob
+import subprocess
+import sys
+
+biceps_version = subprocess.run(["az", "bicep", "version"], stdout=subprocess.PIPE, text=True, shell=True)
+biceps_version = subprocess.run(["az", "bicep", "upgrade"], stdout=subprocess.PIPE, text=True, shell=True)
+#print(biceps_version.stdout)
+
+#print(glob.glob("./**/*.bicep", recursive=True))
+any_error = None
+for bicep_file in glob.glob("./**/*.bicep", recursive=True):
+    result = subprocess.run(["az", "bicep", "build", "--stdout", "--file", bicep_file], shell=True, capture_output=True)
+
+    if result.stderr:
+        print(result.stderr)
+        any_error = True
+
+if any_error:
+    sys.exit(25)
