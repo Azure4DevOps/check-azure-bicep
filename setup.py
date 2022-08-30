572e5e04ed260948ed06c561a4d4ba8e3d30e6c3
Janusz Nowak <janono@gmail.com>
2022-08-30 17:05:52 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,18 @@
-from setuptools import setup
-setup(
+import setuptools
+
+def get_project_requirements() -> str:
+    with open(f"requirements.txt", "r") as f:
+        return f.read()
+
+setuptools.setup(
     name="check-azure-bicep-python",
+    description="check-azure-bicep-python",
+    long_description_content_type="text/markdown",
+    url="https://github.com/Azure4DevOps/check-azure-bicep",
+    python_requires="==3.10.*",
+    license="MIT",
+    packages=setuptools.find_packages(
+        include=["*"],
+    ),
+    install_requires=get_project_requirements(),
 )
