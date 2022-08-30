05867e9b8d9a80ec4b6567d9e43795f1b85b292c
Janusz Nowak <janono@gmail.com>
2022-08-30 18:20:25 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,30 @@
 import setuptools
+import glob
+import subprocess
+import sys
 
 def get_project_requirements() -> str:
     with open(f"requirements.txt", "r") as f:
         return f.read()
+    
+def my_function():
 
+  biceps_version = subprocess.run(["az", "bicep", "version"], stdout=subprocess.PIPE, text=True, shell=True)
+  biceps_version = subprocess.run(["az", "bicep", "upgrade"], stdout=subprocess.PIPE, text=True, shell=True)
+  #print(biceps_version.stdout)
+
+  #print(glob.glob("./**/*.bicep", recursive=True))
+  any_error = None
+  for bicep_file in glob.glob("./**/*.bicep", recursive=True):
+      result = subprocess.run(["az", "bicep", "build", "--stdout", "--file", bicep_file], shell=True, capture_output=True)
+
+      if result.stderr:
+          print(result.stderr)
+          any_error = True
+
+  if any_error:
+      sys.exit(25)
+        
 setuptools.setup(
     name="check-azure-bicep-python",
     description="check-azure-bicep-python",
