632c09a941da4c7e82f23b693d85c92527b3b21f
Janusz Nowak <janono@gmail.com>
2022-08-30 19:30:10 +0200

    Create __init__.py

---
+++ checkazurebiceppython/__init__.py
@@ -0,0 +1,17 @@
+def my_function2():
+
+  biceps_version = subprocess.run(["az", "bicep", "version"], stdout=subprocess.PIPE, text=True, shell=True)
+  biceps_version = subprocess.run(["az", "bicep", "upgrade"], stdout=subprocess.PIPE, text=True, shell=True)
+  #print(biceps_version.stdout)
+
+  #print(glob.glob("./**/*.bicep", recursive=True))
+  any_error = None
+  for bicep_file in glob.glob("./**/*.bicep", recursive=True):
+      result = subprocess.run(["az", "bicep", "build", "--stdout", "--file", bicep_file], shell=True, capture_output=True)
+
+      if result.stderr:
+          print(result.stderr)
+          any_error = True
+
+  if any_error:
+      sys.exit(25)
