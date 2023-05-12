f0a066c7134dd2c807a8c4fee827aa0dcb8fcb14
Janusz Nowak <janono@gmail.com>
2023-05-12 12:12:02 +0200

    Update __init__.py

--- checkazurebiceppython/__init__.py
+++ checkazurebiceppython/__init__.py
@@ -3,38 +3,46 @@ import glob
 import subprocess
 import sys
 
+
 def az_bicep_build():
 
-  biceps_version = subprocess.run(["az", "bicep", "version"], stdout=subprocess.PIPE, text=True, shell=True)
-  #biceps_version = subprocess.run(["az", "bicep", "upgrade"], stdout=subprocess.PIPE, text=True, shell=True)
-  #print(biceps_version.stdout)
+    biceps_version = subprocess.run(
+        ["az", "bicep", "version"], stdout=subprocess.PIPE, text=True, shell=True)
+    biceps_version = subprocess.run(
+        ["az", "bicep", "upgrade"], stdout=subprocess.PIPE, text=True, shell=True)
+    # print(biceps_version.stdout)
 
-  #print(glob.glob("./**/*.bicep", recursive=True))
-  any_error = None
-  for bicep_file in glob.glob("./**/*.bicep", recursive=True):
-      result = subprocess.run(["az", "bicep", "build", "--stdout", "--file", bicep_file], shell=True, capture_output=True)
+    # print(glob.glob("./**/*.bicep", recursive=True))
+    any_error = None
+    for bicep_file in glob.glob("./**/*.bicep", recursive=True):
+        result = subprocess.run(["az", "bicep", "build", "--stdout",
+                                "--file", bicep_file], shell=True, capture_output=True)
 
-      if result.stderr:
-          print(result.stderr)
-          any_error = True
+        if result.stderr:
+            print(result.stderr)
+            any_error = True
 
-  if any_error:
-      sys.exit(25)
+    if any_error:
+        sys.exit(25)
+
 
 def az_bicep_format():
 
-  biceps_version = subprocess.run(["az", "bicep", "version"], stdout=subprocess.PIPE, text=True, shell=True)
-  #biceps_version = subprocess.run(["az", "bicep", "upgrade"], stdout=subprocess.PIPE, text=True, shell=True)
-  #print(biceps_version.stdout)
+    biceps_version = subprocess.run(
+        ["az", "bicep", "version"], stdout=subprocess.PIPE, text=True, shell=True)
+    biceps_version = subprocess.run(
+        ["az", "bicep", "upgrade"], stdout=subprocess.PIPE, text=True, shell=True)
+    # print(biceps_version.stdout)
 
-  #print(glob.glob("./**/*.bicep", recursive=True))
-  any_error = None
-  for bicep_file in glob.glob("./**/*.bicep", recursive=True):
-      result = subprocess.run(["az", "bicep", "format", "--file", bicep_file], shell=False, capture_output=True)
+    # print(glob.glob("./**/*.bicep", recursive=True))
+    any_error = None
+    for bicep_file in glob.glob("./**/*.bicep", recursive=True):
+        result = subprocess.run(
+            ["az", "bicep", "format", "--file", bicep_file], shell=False, capture_output=True)
 
-      if result.stderr:
-          print(result.stderr)
-          any_error = True
+        if result.stderr:
+            print(result.stderr)
+            any_error = True
 
-  if any_error:
-      sys.exit(25)
+    if any_error:
+        sys.exit(25)
